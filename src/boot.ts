21de2a8f434225c60658cc5efdcb357dca619ab6
GongT <admin@gongt.me>
2017-05-12 12:19:05 +0800

    update package

--- src/boot.ts
+++ src/boot.ts
@@ -1,8 +1,11 @@
-///<reference path="./globals.d.ts"/>
+/// <reference path="./globals.d.ts"/>
+
 import "source-map-support/register";
 import "@gongt/jenv-data/global";
 
-
+import {initDefaultDatabaseConnection} from "typescript-common-library/server/database/mongodb";
+export const defaultDatabaseConnectionString = JsonEnv.DataBaseUrlTemplate.replace('%DATABASE-NAME%', 'DefaultDatabase');
+initDefaultDatabaseConnection(defaultDatabaseConnectionString);
 
 global['IS_CLIENT'] = false;
 global['IS_DEBUG'] = true;
