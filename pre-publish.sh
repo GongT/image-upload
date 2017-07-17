77d1957b8a5c8e35168d567c45708bdf68397e50
GongT <admin@gongt.me>
2017-07-17 13:37:29 +0800

    update version

--- pre-publish.sh
+++ pre-publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 tsc -p src/server
 tsc -p src/package
@@ -9,4 +10,4 @@ tsc -p src/package/tsconfig.es5.json
 unlink dist/npm-package/package.json || true
 cp src/package/package.json dist/npm-package/package.json
 cd dist/npm-package
-npm publish --access=public
+# npm publish --access=public
